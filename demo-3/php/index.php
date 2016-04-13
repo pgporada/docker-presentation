ebaf98adf5d946763bb797c4abf8a47a55e18c14
Phil Porada <philporada@gmail.com>
2016-04-13 03:52:59 -0400

    updated presentation

---
+++ demo-3/php/index.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>PHP Site</title>
+    </head>
+    <body>
+    <h1>
+    I am HTML!
+    <br>
+    <?php
+    print "I am PHP!\n";
+    ?>
+    </h1>
+    <br>
+    <h3>
+    <?php
+    echo 'Current PHP version: ' . phpversion();
+    ?>
+    </h3>
+    <h4>Containers lol</h4>
+    <img src='http://theinstructionlimit.com/wp-content/uploads/2015/02/fsn1.gif'></img>
+    <br>
+    <img src='https://mcfliptables.files.wordpress.com/2015/07/higherqualityrainbowpukegifactivate_27be1bc0d5d8df2f81400e34a92b38f8.gif'></img>
+    </body>
+</html>
